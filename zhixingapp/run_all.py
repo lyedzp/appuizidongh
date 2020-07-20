b1425447c47fe1fd40b3234b3e99afa293aba109
1@qq.com <1@qq.com>
2020-07-20 22:19:44 +0800

    提交信息

---
+++ zhixingapp/run_all.py
@@ -0,0 +1,16 @@
+import time
+from HTMLTestRunner import HTMLTestRunner
+from unittest import defaultTestLoader
+
+# 指定测试用例为当前文件夹下的interface目录
+test_dir = './case'
+testsuit = defaultTestLoader.discover(test_dir,pattern='test*')
+if __name__=='__main__':
+    now = time.strftime("%Y-%m-%d %H_%M_%S")
+    report_name='./report/'+now+'result.html'
+    fp = open(report_name,'wb')
+    runner = HTMLTestRunner(stream=fp,
+                            title='appui自动化测试',
+                            description='运行环境')
+    runner.run(testsuit)
+    fp.close()
